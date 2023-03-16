b4527d126f2fe7efc51e426f91c00690f1681758
Godson <godsonokutu555@gmail.com>
2023-03-16 14:36:29 -0700

    a C program that prints with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ * main - C program that prints output with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
