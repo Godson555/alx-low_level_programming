41a6a377f30633b2ea7060a0ff677f59e8d37fd9
Godson <godsonokutu555@gmail.com>
2023-03-17 14:40:26 -0700

    program that prints numbers in base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: 'Program that prints single digit numbers in base 10'
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
