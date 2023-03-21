46989e5e4f3e0bc0e32b4a5e1b74604eb5ebfb4b
Godson <godsonokutu555@gmail.com>
2023-03-21 05:12:33 -0700

    Function that print string _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main -Entry point
- * Description: 'Program that prints a string called _putchar'
+ * Description: ' Program that print alphabet'
  * Return: 0
  */
 int main(void)
 {
-	puts("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
