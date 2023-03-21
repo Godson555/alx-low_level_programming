881db6050d2fa3b95f1ca26e409e6743248b200b
Godson <godsonokutu555@gmail.com>
2023-03-21 01:58:39 -0700

    Program that prints string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: 'Program that prints a string called _putchar'
+ * Return: 0
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
