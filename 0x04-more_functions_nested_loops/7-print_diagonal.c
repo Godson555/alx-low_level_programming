b67a80e30a82ea0e8d3f785b9988c8db7fd0f026
Godson <godsonokutu555@gmail.com>
2023-03-23 15:30:51 -0700

    yes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,18 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (a = 0; a < n; a++)
-		{
-			for (b = 0; b < n; b++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
-
+	for (a = 0; a < n; a++)
+	{
+	for (b = 0; b < a; b++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
 	}
 }
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: "to print FizzBuzz"
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FzzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
