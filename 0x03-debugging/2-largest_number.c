0e26759ef413b73bb9999dde0223398742de02bc
Godson <godsonokutu555@gmail.com>
2023-03-23 01:26:08 -0700

    debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,9 +24,9 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b > c && c > a)
 		largest = b;
-	else if (b > c && c > a)
+	else if (c > b && b > a)
 		largest = c;
-	else
+	else if (c > a && a > b)
 	{
 	largest = c;
 	}
