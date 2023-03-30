e601247c0008d864aced2ce77a3c0607f3f5b1db
Godson <godsonokutu555@gmail.com>
2023-03-30 03:57:18 -0700

    more pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,10 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
-
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 
 
