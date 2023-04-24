37be3599b12c953a181f8f9e5cec9d936cafd8c6
Godson <godsonokutu555@gmail.com>
2023-04-24 16:01:26 -0700

    minimum

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
- * argc: argument count
- * argv: argument vector
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
 int main(int argc, char *argv[])
