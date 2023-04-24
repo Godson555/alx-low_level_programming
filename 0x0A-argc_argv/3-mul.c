f7150d09c316133cf3e7f9ceecf6fd29c236c3aa
Godson <godsonokutu555@gmail.com>
2023-04-24 21:40:28 +0000

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,56 +2,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-<<<<<<< HEAD
  * main - print the multiplication of two integers
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 or 1
-=======
- * _atoi - convert a string to an integer
- * @s: p1
- * Return: 0
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == ' ')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (n);
-}
-/**
- * main - Entry point
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0
->>>>>>> 7a19330b9ecc30d5d8a854b91b8388e247d554c9
  */
 int main(int argc, char *argv[])
 {
